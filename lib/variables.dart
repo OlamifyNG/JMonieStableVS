c0153db442ac23e8f9861b8578f49a6eb5bf2983
Olamide2 <olamify1@gmail.com>
2024-12-23 02:21:35 +0000

    Initial commit

---
+++ lib/variables.dart
@@ -0,0 +1,8 @@
+String _uProfilePic =
+    "https://lh3.googleusercontent.com/a/ACg8ocK_USa6dAJuus5ln5sCQ0FNQ4ryC9pCX2k-5RVW0UjRP2c0tmVC=s360-c-no";
+// variables.dart
+
+// Public getter to access the private variable
+String get uProfilePic => _uProfilePic;
+
+String uBalance = '₦' '2,587.00';
